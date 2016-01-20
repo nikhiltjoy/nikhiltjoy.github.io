64404499b030158c87a936f6572fce008e07a57c
Nikhil Thomas Joy <njoy@uwaterloo.ca>
2016-01-19 22:46:28 -0500

    updated logos, and made images less quality

--- assets/js/grayscale.js
+++ assets/js/grayscale.js
@@ -1,2 +1,233 @@
-$(function(){var cRatio={height:0,width:0};var lRatio;var cArray=["rgb(255,255,255)","rgb(0,0,0)","rgb(0,0,0)","rgb(255,255,255)"]
-$(window).scroll(function(){if($(".navbar").offset().top>50){$(".navbar-fixed-top").addClass("top-nav-collapse");$('#timeline').height($(window).height()-$('#navBar').height());$(".textNav").css("color",cArray[0]);}else{$(".navbar-fixed-top").removeClass("top-nav-collapse");$('#timeline').height($(window).height());var index=$('#myCarousel').find('.active').index();$(".textNav").css("color",cArray[index]);}});$(window).load(function(){if($(".navbar").offset().top>50){$(".navbar-fixed-top").addClass("top-nav-collapse");}else{$(".navbar-fixed-top").removeClass("top-nav-collapse");}var iHeight=$(window).height()*0.55-$('#button').height();var iWidth=$(window).width()*0.7;var dim=Math.min(iHeight,iWidth);var offset=parseFloat($('.hover').css('margin-bottom'));offset=(offset===0)?($('#caption').height()+$('#button').height()*1.25):(offset-$('#button').height()/2);$('#introContainer').height(dim).width(dim);$('#button').css('bottom',offset);$('#timeline').height($(window).height()-$('#navBar').height());$('#logo').height($('#introContainer').height()*0.76).width($('#introContainer').height()*0.76);$('#logoContainer').height($('#introContainer').height()*0.76).width($('#introContainer').height()*0.76);$('#introContainer').css('left',$(window).width()*585/1280);$('#button').css('left',$(window).width()/2);});$(window).resize(function(){var iHeight=$(window).height()*0.55-$('#button').height();var iWidth=$(window).width()*0.7;var dim=Math.min(iHeight,iWidth);var offset=parseFloat($('.hover').css('margin-bottom'));offset=(offset===0)?($('#caption').height()+$('#button').height()*1.25):(offset-$('#button').height()/2);$('#introContainer').height(dim).width(dim);$('#timeline').height($(window).height());$('#button').css('bottom',offset);$('#logo').height($('#introContainer').height()*0.76).width($('#introContainer').height()*0.76);$('#logoContainer').height($('#introContainer').height()*0.76).width($('#introContainer').height()*0.76);$('#introContainer').css('left',$(window).width()*585/1280);$('#button').css('left',$(window).width()/2);if($(window).width()>=768&&$(".navbar").offset().top<50){var index=$('#myCarousel').find('.active').index();$(".textColor").css('color',cArray[index]);}else{$(".textColor").css({color:"#fff"});}});});$(function(){$('a.page-scroll').bind('click',function(event){var $anchor=$(this);$('html, body').stop().animate({scrollTop:$($anchor.attr('href')).offset().top},1500,'easeInOutExpo');event.preventDefault();});});$('#myCarousel').carousel({interval:5000,cycle:true});$('#myCarousel').on('slide.bs.carousel',function(e){var index=$(this).find('.active').index();var slideTo=$(e.relatedTarget).index();if($(window).width()>=768&&$(".navbar").offset().top<50){if(slideTo===0||slideTo===3){$(".textColor").animate({color:"#fff"},600);}else{$(".textColor").animate({color:"#000"},600);}}else{$(".textColor").css({color:"#fff"});}if(slideTo%2===0){$(".intro-body-text").animate({backgroundColor:"rgba(0, 0, 0, 0.4)"},600);}else{$(".intro-body-text").animate({backgroundColor:"rgba(255, 255, 255, 0.4)"},600);}});$('.navbar-collapse ul li a').click(function(){$('.navbar-toggle:visible').click();});google.maps.event.addDomListener(window,'load',init);function init(){var mapOptions={zoom:15,center:new google.maps.LatLng(40.6700,-73.9400),disableDefaultUI:true,scrollwheel:false,draggable:false,styles:[{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]}]};var mapElement=document.getElementById('map');var map=new google.maps.Map(mapElement,mapOptions);var image='img/map-marker.png';var myLatLng=new google.maps.LatLng(40.6700,-73.9400);var beachMarker=new google.maps.Marker({position:myLatLng,map:map,icon:image});}
+$(function() {
+    var cRatio = {
+        height: 0,
+        width: 0
+    };
+    var lRatio;
+    var cArray = ["rgb(255,255,255)", "rgb(0,0,0)", "rgb(0,0,0)", "rgb(255,255,255)"]
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+            $('#timeline').height($(window).height() - $('#navBar').height());
+            $(".textNav").css("color", cArray[0]);
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+            $('#timeline').height($(window).height());
+            var index = $('#myCarousel').find('.active').index();
+            $(".textNav").css("color", cArray[index]);
+        }
+    });
+    $(window).load(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+        var iHeight = $(window).height() * 0.55 - $('#button').height();
+        var iWidth = $(window).width() * 0.7;
+        var dim = Math.min(iHeight, iWidth);
+        var offset = parseFloat($('.hover').css('margin-bottom'));
+        offset = (offset === 0) ? ($('#caption').height() + $('#button').height() * 1.25) : (offset - $('#button').height() / 2);
+        $('#introContainer').height(dim).width(dim);
+        $('#button').css('bottom', offset);
+        $('#timeline').height($(window).height() - $('#navBar').height());
+        $('#logo').height($('#introContainer').height() * 0.76).width($('#introContainer').height() * 0.76);
+        $('#logoContainer').height($('#introContainer').height() * 0.76).width($('#introContainer').height() * 0.76);
+        $('#introContainer').css('left', $(window).width() * 585 / 1280);
+        $('#button').css('left', $(window).width() / 2);
+    });
+    $(window).resize(function() {
+        var iHeight = $(window).height() * 0.55 - $('#button').height();
+        var iWidth = $(window).width() * 0.7;
+        var dim = Math.min(iHeight, iWidth);
+        var offset = parseFloat($('.hover').css('margin-bottom'));
+        offset = (offset === 0) ? ($('#caption').height() + $('#button').height() * 1.25) : (offset - $('#button').height() / 2);
+        $('#introContainer').height(dim).width(dim);
+        $('#timeline').height($(window).height());
+        $('#button').css('bottom', offset);
+        $('#logo').height($('#introContainer').height() * 0.76).width($('#introContainer').height() * 0.76);
+        $('#logoContainer').height($('#introContainer').height() * 0.76).width($('#introContainer').height() * 0.76);
+        $('#introContainer').css('left', $(window).width() * 585 / 1280);
+        $('#button').css('left', $(window).width() / 2);
+        if ($(window).width() >= 768 && $(".navbar").offset().top < 50) {
+            var index = $('#myCarousel').find('.active').index();
+            $(".textColor").css('color', cArray[index]);
+        } else {
+            $(".textColor").css({
+                color: "#fff"
+            });
+        }
+    });
+});
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+$('#myCarousel').carousel({
+    interval: 5000,
+    cycle: true
+});
+$('#myCarousel').on('slide.bs.carousel', function(e) {
+    var index = $(this).find('.active').index();
+    var slideTo = $(e.relatedTarget).index();
+    if ($(window).width() >= 768 && $(".navbar").offset().top < 50) {
+        if (slideTo === 0 || slideTo === 3) {
+            $(".textColor").animate({
+                color: "#fff"
+            }, 600);
+        } else {
+            $(".textColor").animate({
+                color: "#000"
+            }, 600);
+        }
+    } else {
+        $(".textColor").css({
+            color: "#fff"
+        });
+    }
+    if (slideTo % 2 === 0) {
+        $(".intro-body-text").animate({
+            backgroundColor: "rgba(0, 0, 0, 0.4)"
+        }, 600);
+    } else {
+        $(".intro-body-text").animate({
+            backgroundColor: "rgba(255, 255, 255, 0.4)"
+        }, 600);
+    }
+});
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+    var mapOptions = {
+        zoom: 15,
+        center: new google.maps.LatLng(40.6700, -73.9400),
+        disableDefaultUI: true,
+        scrollwheel: false,
+        draggable: false,
+        styles: [{
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#000000"
+            }, {
+                "lightness": 17
+            }]
+        }, {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#000000"
+            }, {
+                "lightness": 20
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#000000"
+            }, {
+                "lightness": 17
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "color": "#000000"
+            }, {
+                "lightness": 29
+            }, {
+                "weight": 0.2
+            }]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#000000"
+            }, {
+                "lightness": 18
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#000000"
+            }, {
+                "lightness": 16
+            }]
+        }, {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#000000"
+            }, {
+                "lightness": 21
+            }]
+        }, {
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+                "visibility": "on"
+            }, {
+                "color": "#000000"
+            }, {
+                "lightness": 16
+            }]
+        }, {
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "saturation": 36
+            }, {
+                "color": "#000000"
+            }, {
+                "lightness": 40
+            }]
+        }, {
+            "elementType": "labels.icon",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "transit",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#000000"
+            }, {
+                "lightness": 19
+            }]
+        }, {
+            "featureType": "administrative",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#000000"
+            }, {
+                "lightness": 20
+            }]
+        }, {
+            "featureType": "administrative",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "color": "#000000"
+            }, {
+                "lightness": 17
+            }, {
+                "weight": 1.2
+            }]
+        }]
+    };
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var image = 'img/map-marker.png';
+    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var beachMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: image
+    });
+}
--- assets/js/new_script.js
+++ assets/js/new_script.js
@@ -14,10 +14,10 @@ $(function(){var dataObject ={
                 "startDate": "2011,9,5",
                 "endDate": "2016,4,22",
                 "headline": "My Waterloo Journey Begins",
-                "text": "I started my undergraduate career, after doing high school in Abu Dhabi, Chennai, and Mississauga. Currently pursuing my Electrical Engineering degree with a Computer Engineering Option.",
+                "text": "I started my undergraduate career, after doing high school in Abu Dhabi, Chennai, and Toronto. Currently pursuing my Electrical Engineering degree with a Computer Engineering Option.",
                 "asset": {
-                    "media": "https://uwaterloo.ca/brand-guidelines/sites/ca.brand-guidelines/files/resize/uploads/images/waterloo_eng_electric_comp_logo_rgb-300x103.png",
-                    "thumbnail": "http://www.wec.uwaterloo.ca/files/images/logos/EngSoc_Logo.jpg",
+                    "media": "./assets/img/uw_ece_logo.png",
+                    "thumbnail": "./assets/img/uw_ece_thumb.png",
                     "credit": "University of Waterloo",
                     "caption": ""
                 }
@@ -28,8 +28,8 @@ $(function(){var dataObject ={
                 "headline": "Mutimedia Application Developer - University of Waterloo",
                 "text": "Location: Waterloo, ON \u25baTook charge of the smartphone component of the project, comprising of design aspects of front-end development in ActionScript such as audio-video streaming, SMS, and email features\u25baLearned Java and XCode for native implementation on both iOS and Android while using SVN repositories to monitor project progress",
                 "asset": {
-                    "media": "https://uwaterloo.ca/brand-guidelines/sites/ca.brand-guidelines/files/resize/uploads/images/universityofwaterloo_logo_horiz_rgb_0-300x120.png",
-                    "thumbnail": "http://www.bulletin.uwaterloo.ca/images/2009/0724UWlogoceremonial.jpg",
+                    "media": "./assets/img/uw_logo.png",
+                    "thumbnail": "./assets/img/uw_thumb.png",
                     "credit": "University of Waterloo",
                     "caption": ""
                 }
@@ -40,8 +40,8 @@ $(function(){var dataObject ={
                 "headline": "Power System Model Management Intern - AESO",
                 "text": "Location: Calgary, AB \u25baUsed modelling software such as PSS/E to solve various base cases \u25baMaintained Power System database acquiring experience in MS Access, and SQL \u25baPrepared Auxiliary Data and Errata files for published Planning Base Case Suites by authoring custom Python scripts to automate and optimize company procedures by tenfold",
                 "asset": {
-                    "media": "http://www.aeso.ca/images/logo.gif",
-                    "thumbnail": "http://media.glassdoor.com/sqll/156469/alberta-electric-system-operator-squarelogo-1424703338456.png",
+                    "media": "./assets/img/aeso_logo.png",
+                    "thumbnail": "./assets/img/aeso_thumb.png",
                     "credit": "Alberta Electric Systems Operator",
                     "caption": ""
                 }
@@ -52,8 +52,8 @@ $(function(){var dataObject ={
                 "headline": "Power System Modelling Intern - BC Hydro",
                 "text": "Location: Burnaby, BC \u25baWorked in the Power System Modelling Team, Transmission and Distribution, and enhancing in-house developed software for the generation of customized base cases for both operations and planning \u25baUndertook the responsibility of updating the model of the Albertan electrical system, such that it can be effectively incorporated in future WECC base cases. \u25baUsed PSS/E, and knowledge of user extensions, as well as command line prompts of PSS/E to further develop the in-house \u201cBase Case Generator.\u201d",
                 "asset": {
-                    "media": "http://104.236.16.159/wp-content/uploads/2009/12/BC_Hydro-logo.jpg",
-                    "thumbnail": "https://pbs.twimg.com/profile_images/480089811890348033/S90p7jig_400x400.png",
+                    "media": "./assets/img/bch_logo.png",
+                    "thumbnail": "./assets/img/bch_thumb.png",
                     "credit": "BC Hydro",
                     "caption": ""
                 }
@@ -64,8 +64,8 @@ $(function(){var dataObject ={
                 "headline": "MATH 119 Teaching Assistant - University of Waterloo",
                 "text": "Location: Waterloo, ON \u25baPart time teaching assistant for MATH 119 (Calculus 2) course, and in charge of running help sessions and tutoring for the course.",
                 "asset": {
-                    "media": "https://uwaterloo.ca/brand-guidelines/sites/ca.brand-guidelines/files/resize/uploads/images/universityofwaterloo_logo_horiz_rgb_0-300x120.png",
-                    "thumbnail": "http://www.bulletin.uwaterloo.ca/images/2009/0724UWlogoceremonial.jpg",
+                    "media": "./assets/img/uw_logo.png",
+                    "thumbnail": "./assets/img/uw_thumb.png",
                     "credit": "University of Waterloo",
                     "caption": ""
                 }
@@ -76,8 +76,8 @@ $(function(){var dataObject ={
                 "headline": "Systems Engineering Associate - Endura Energy",
                 "text": "Location: Toronto, ON \u25baGenerated PVSyst simulations to simulate shading and power production with solar energy projects (rooftop and ground-mount) \u25baDesigned systems using AutoCAD to fulfill Connection Impact Assessment and Electrical Safety Authority Submissions \u25baConducted research on impact of snowfall, ambient temperature, and geographical location to determine soiling losses on power production \u25baHeaded Database development using Google Docs, and Google App Script to meet project specifications",
                 "asset": {
-                    "media": "http://cansia.ca/sites/default/files/imagecache/organization_logo_200/uploads/org_logos/endura-energy.png",
-                    "thumbnail": "http://cansia.ca/sites/default/files/imagecache/organization_logo_200/uploads/org_logos/endura-energy.png",
+                    "media": "./assets/img/endura_logo.png",
+                    "thumbnail": "./assets/img/endura_thumb.png",
                     "credit": "Endura Energy Project Corporation",
                     "caption": ""
                 }
@@ -88,8 +88,8 @@ $(function(){var dataObject ={
                 "headline": "International Student Exchange - Lund University",
                 "text": "Location: Lund, Sk\u00e5ne, Sweden \u25baPursued A1 Certification in Swedish \u25baUndertook Electric Power Systerms, Digital Communications, and Advanced Digital Communications \u25baTravelled throughout Europe, visited 12 different countries, and in turn, discovered myself \u25baStrived to learn one phrase in as many languages as possible. Till date: 11 Languages :)",
                 "asset": {
-                    "media": "http://www.lu.se/sites/www.lu.se/files/logo.png",
-                    "thumbnail": "http://upload.wikimedia.org/wikipedia/en/thumb/5/5c/Lund_University_seal.svg/32px-Lund_University_seal.svg.png",
+                    "media": "./assets/img/lund_logo.png",
+                    "thumbnail": "./assets/img/lund_thumb.png",
                     "credit": "Lund University",
                     "caption": ""
                 }
@@ -100,8 +100,8 @@ $(function(){var dataObject ={
                 "headline": "Software Engineering Intern - Seeq Corporation",
                 "text": "Location: Remote \u25baSpearheaded effort to integrate manual tests into testing framework using ideologies from Behaviour Driven Development utilizing Node.js, Cucumber.js, Embedded JavaScript (EJS), Bootstrap, and jQuery \u25baAuthored scripts using Node.js to dynamically query JIRA for completed tickets for automated Release Notes generation with EJS when product executable is built \u25baIntegrated Flux-Angular into portions of Seeq\u2019s Angular application for \u201cstate\u201d-ful data flow \u25baAddressed various bugs in Seeq\u2019s application front-end, and added Jasmine Unit Tests to improve overall code coverage",
                 "asset": {
-                    "media": "http://i.imgur.com/hBhCybo.png",
-                    "thumbnail": "http://i.imgur.com/hBhCybo.png",
+                    "media": "./assets/img/seeq_logo.png",
+                    "thumbnail": "./assets/img/seeq_thumb.png",
                     "credit": "Seeq Corporation",
                     "caption": ""
                 }
@@ -112,8 +112,8 @@ $(function(){var dataObject ={
                 "headline": "Software Engineering Intern - Bustle",
                 "text": "Location: New York, NY \u25baSpearheaded infrastructure development to collect various metrics such as click events, app installation instances, etc. using Amazon\u2019s API Gateway and Lambda (node.js) in main Ember.js application \u25baTook charge of project that built infrastructure using ElasticSearch and Serverless, a framework that utilizes AWS to build serverless micro-services, to handle geo-ip requests, and further provide weather data and location based analytics for internal use",
                 "asset": {
-                    "media": "http://cdn.bustle.com/images/logo-2242a9475cf9232975c867daa229715e.svg",
-                    "thumbnail": "http://www.bustle.com/ms-tile-144.png",
+                    "media": "./assets/img/bustle_logo.png",
+                    "thumbnail": "./assets/img/bustle_thumb.png",
                     "credit": "Bustle",
                     "caption": ""
                 }
