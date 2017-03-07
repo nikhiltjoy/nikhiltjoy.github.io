29ad60fbe3fa9fa0b997f27a8bbc44b2408981f0
Nikhil Thomas Joy <njoy@uwaterloo.ca>
2017-03-07 16:46:02 -0500

    updated work info

--- assets/js/new_script.js
+++ assets/js/new_script.js
@@ -1,6 +1,6 @@
 $(function(){var dataObject ={
     "timeline": {
-        "headline": "My Undergraduate Endeavours",
+        "headline": "My Professional Endeavours",
         "type": "default",
         "startDate": "2011,9,5",
         "text": "Flip through for a timeline of my work experience and other adventures",
@@ -26,7 +26,7 @@ $(function(){var dataObject ={
                 "startDate": "2012,4,30",
                 "endDate": "2012,8,31",
                 "headline": "Mutimedia Application Developer - University of Waterloo",
-                "text": "Location: Waterloo, ON \u25baTook charge of the smartphone component of the project, comprising of design aspects of front-end development in ActionScript such as audio-video streaming, SMS, and email features\u25baLearned Java and XCode for native implementation on both iOS and Android while using SVN repositories to monitor project progress",
+                "text": "Location: Waterloo, ON <br>\u25baTook charge of the smartphone component of the project, comprising of design aspects of front-end development in ActionScript such as audio-video streaming, SMS, and email features<br>\u25baLearned Java and XCode for native implementation on both iOS and Android while using SVN repositories to monitor project progress",
                 "asset": {
                     "media": "./assets/img/uw_logo.png",
                     "thumbnail": "./assets/img/uw_thumb.png",
@@ -38,7 +38,7 @@ $(function(){var dataObject ={
                 "startDate": "2013,1,7",
                 "endDate": "2013,4,26",
                 "headline": "Power System Model Management Intern - AESO",
-                "text": "Location: Calgary, AB \u25baUsed modelling software such as PSS/E to solve various base cases \u25baMaintained Power System database acquiring experience in MS Access, and SQL \u25baPrepared Auxiliary Data and Errata files for published Planning Base Case Suites by authoring custom Python scripts to automate and optimize company procedures by tenfold",
+                "text": "Location: Calgary, AB <br>\u25baUsed modelling software such as PSS/E to solve various base cases <br>\u25baMaintained Power System database acquiring experience in MS Access, and SQL <br>\u25baPrepared Auxiliary Data and Errata files for published Planning Base Case Suites by authoring custom Python scripts to automate and optimize company procedures by tenfold",
                 "asset": {
                     "media": "./assets/img/aeso_logo.png",
                     "thumbnail": "./assets/img/aeso_thumb.png",
@@ -50,7 +50,7 @@ $(function(){var dataObject ={
                 "startDate": "2013,9,3",
                 "endDate": "2013,12,20",
                 "headline": "Power System Modelling Intern - BC Hydro",
-                "text": "Location: Burnaby, BC \u25baWorked in the Power System Modelling Team, Transmission and Distribution, and enhancing in-house developed software for the generation of customized base cases for both operations and planning \u25baUndertook the responsibility of updating the model of the Albertan electrical system, such that it can be effectively incorporated in future WECC base cases. \u25baUsed PSS/E, and knowledge of user extensions, as well as command line prompts of PSS/E to further develop the in-house \u201cBase Case Generator.\u201d",
+                "text": "Location: Burnaby, BC <br>\u25baWorked in the Power System Modelling Team, Transmission and Distribution, and enhancing in-house developed software for the generation of customized base cases for both operations and planning <br>\u25baUndertook the responsibility of updating the model of the Albertan electrical system, such that it can be effectively incorporated in future WECC base cases. <br>\u25baUsed PSS/E, and knowledge of user extensions, as well as command line prompts of PSS/E to further develop the in-house \u201cBase Case Generator.\u201d",
                 "asset": {
                     "media": "./assets/img/bch_logo.png",
                     "thumbnail": "./assets/img/bch_thumb.png",
@@ -62,7 +62,7 @@ $(function(){var dataObject ={
                 "startDate": "2014,1,6",
                 "endDate": "2014,4,18",
                 "headline": "MATH 119 Teaching Assistant - University of Waterloo",
-                "text": "Location: Waterloo, ON \u25baPart time teaching assistant for MATH 119 (Calculus 2) course, and in charge of running help sessions and tutoring for the course.",
+                "text": "Location: Waterloo, ON <br>\u25baPart time teaching assistant for MATH 119 (Calculus 2) course, and in charge of running help sessions and tutoring for the course.",
                 "asset": {
                     "media": "./assets/img/uw_logo.png",
                     "thumbnail": "./assets/img/uw_thumb.png",
@@ -74,7 +74,7 @@ $(function(){var dataObject ={
                 "startDate": "2014,4,28",
                 "endDate": "2014,8,15",
                 "headline": "Systems Engineering Associate - Endura Energy",
-                "text": "Location: Toronto, ON \u25baGenerated PVSyst simulations to simulate shading and power production with solar energy projects (rooftop and ground-mount) \u25baDesigned systems using AutoCAD to fulfill Connection Impact Assessment and Electrical Safety Authority Submissions \u25baConducted research on impact of snowfall, ambient temperature, and geographical location to determine soiling losses on power production \u25baHeaded Database development using Google Docs, and Google App Script to meet project specifications",
+                "text": "Location: Toronto, ON <br>\u25baGenerated PVSyst simulations to simulate shading and power production with solar energy projects (rooftop and ground-mount) <br>\u25baDesigned systems using AutoCAD to fulfill Connection Impact Assessment and Electrical Safety Authority Submissions <br>\u25baConducted research on impact of snowfall, ambient temperature, and geographical location to determine soiling losses on power production <br>\u25baHeaded Database development using Google Docs, and Google App Script to meet project specifications",
                 "asset": {
                     "media": "./assets/img/endura_logo.png",
                     "thumbnail": "./assets/img/endura_thumb.png",
@@ -86,7 +86,7 @@ $(function(){var dataObject ={
                 "startDate": "2014,8,18",
                 "endDate": "2015,1,17",
                 "headline": "International Student Exchange - Lund University",
-                "text": "Location: Lund, Sk\u00e5ne, Sweden \u25baPursued A1 Certification in Swedish \u25baUndertook Electric Power Systerms, Digital Communications, and Advanced Digital Communications \u25baTravelled throughout Europe, visited 12 different countries, and in turn, discovered myself \u25baStrived to learn one phrase in as many languages as possible. Till date: 11 Languages :)",
+                "text": "Location: Lund, Sk\u00e5ne, Sweden <br>\u25baPursued A1 Certification in Swedish <br>\u25baUndertook Electric Power Systerms, Digital Communications, and Advanced Digital Communications <br>\u25baTravelled throughout Europe, visited 12 different countries, and in turn, discovered myself <br>\u25baStrived to learn one phrase in as many languages as possible. Till date: 11 Languages :)",
                 "asset": {
                     "media": "./assets/img/lund_logo.png",
                     "thumbnail": "./assets/img/lund_thumb.png",
@@ -98,7 +98,7 @@ $(function(){var dataObject ={
                 "startDate": "2015,1,26",
                 "endDate": "2015,7,15",
                 "headline": "Software Engineering Intern - Seeq Corporation",
-                "text": "Location: Remote \u25baSpearheaded effort to integrate manual tests into testing framework using ideologies from Behaviour Driven Development utilizing Node.js, Cucumber.js, Embedded JavaScript (EJS), Bootstrap, and jQuery \u25baAuthored scripts using Node.js to dynamically query JIRA for completed tickets for automated Release Notes generation with EJS when product executable is built \u25baIntegrated Flux-Angular into portions of Seeq\u2019s Angular application for \u201cstate\u201d-ful data flow \u25baAddressed various bugs in Seeq\u2019s application front-end, and added Jasmine Unit Tests to improve overall code coverage",
+                "text": "Location: Remote <br>\u25baSpearheaded effort to integrate manual tests into testing framework using ideologies from Behaviour Driven Development utilizing Node.js, Cucumber.js, Embedded JavaScript (EJS), Bootstrap, and jQuery <br>\u25baAuthored scripts using Node.js to dynamically query JIRA for completed tickets for automated Release Notes generation with EJS when product executable is built <br>\u25baIntegrated Flux-Angular into portions of Seeq\u2019s Angular application for \u201cstate\u201d-ful data flow <br>\u25baAddressed various bugs in Seeq\u2019s application front-end, and added Jasmine Unit Tests to improve overall code coverage",
                 "asset": {
                     "media": "./assets/img/seeq_logo.png",
                     "thumbnail": "./assets/img/seeq_thumb.png",
@@ -110,11 +110,35 @@ $(function(){var dataObject ={
                 "startDate": "2015,9,1",
                 "endDate": "2015,12,17",
                 "headline": "Software Engineering Intern - Bustle",
-                "text": "Location: New York, NY \u25baSpearheaded infrastructure development to collect various metrics such as click events, app installation instances, etc. using Amazon\u2019s API Gateway and Lambda (node.js) in main Ember.js application \u25baTook charge of project that built infrastructure using ElasticSearch and Serverless, a framework that utilizes AWS to build serverless micro-services, to handle geo-ip requests, and further provide weather data and location based analytics for internal use",
+                "text": "Location: New York, NY <br>\u25baSpearheaded infrastructure development to collect various metrics such as click events, app installation instances, etc. using Amazon\u2019s API Gateway and Lambda (node.js) in main Ember.js application <br>\u25baTook charge of project that built infrastructure using ElasticSearch and Serverless, a framework that utilizes AWS to build serverless micro-services, to handle geo-ip requests, and further provide weather data and location based analytics for internal use",
                 "asset": {
                     "media": "./assets/img/bustle_logo.png",
                     "thumbnail": "./assets/img/bustle_thumb.png",
                     "credit": "Bustle",
+                    "caption": ""
+                }
+            },
+            {
+                "startDate": "2016,1,6",
+                "endDate": "2016,4,23",
+                "headline": "Undergraduate Research Assistant",
+                "text": "Location: Waterloo, ON <br>\u25baWorked with Prof. Mark Crowley on Insecure Activity Pattern Detection, employing Machine Learning models to detect anomalous usage, and designed front-end user interface",
+                "asset": {
+                    "media": "./assets/img/uw_logo.png",
+                    "thumbnail": "./assets/img/uw_thumb.png",
+                    "credit": "University of Waterloo",
+                    "caption": ""
+                }
+            },
+            {
+                "startDate": "2016,6,20",
+                "endDate": "",
+                "headline": "Data Scientist - Capital One",
+                "text": "Location: Toronto, ON; London, UK <br>\u25baCurrently on Digital team in Canada, delving into metrics obtained from digital channels to drive higher mobile app downloads<br>\u25baEngineered a data visualization dashboard using D3,js, Angular.js, Flask, and Teradata SQL; revitalizing the existing Tableau version<br>\u25baDeveloped a geographically themed monitoring dashboard on Tableau using R, and UK GIS Data to determine residuals of internal models from UK postcodes<br>\u25baOverhauled existing UK credit bureau data monitoring scripts from SAS to R, involving comparative analysis of historic data and ensuring that recent data follows historic trends. Output reports were generated using Knitr and ggplot2",
+                "asset": {
+                    "media": "./assets/img/c1_logo.png",
+                    "thumbnail": "./assets/img/c1_thumb.png",
+                    "credit": "Capital One",
                     "caption": ""
                 }
             }
